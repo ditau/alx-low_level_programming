35e4d862f823193a5ffefedf8e085227c1fabd3b
ditau <ditaumolatedi@gmail.com>
2023-07-13 16:41:11 -0700

    C_is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <uunistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,5 +10,5 @@ int main (void)
 {
 	write(2,
 		"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-		return(0);
+		return(1);
 }
