75756213f50e04100b72861f47b43857a86f4619
ditau <ditaumolatedi@gmail.com>
2023-07-19 15:09:52 -0700

    Floops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,13 +3,16 @@
 /**
  * main - prints first 50 Fibonacci numbers, starting with 1 and 2,
  * separated by a comma, followed by space
- * then followed by a new line
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
@@ -18,7 +21,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 
-		if (count ==49)
+		if (count == 49)
 			printf("\n");
 		else
 			printf(",");
